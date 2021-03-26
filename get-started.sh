80fdfd0e83ac881a58a9efb9a8c16719b04cad88
Wan Bachtiar <sindbach@gmail.com>
2021-03-26 16:36:20 +1100

    Updated to use GCR

--- get-started.sh
+++ get-started.sh
@@ -5,10 +5,10 @@ then
     read -p "MONGODB URI (Required): " MONGODB_URI
 fi 
 
-DRIVER_VERSION=${2:-2.11.5}
+DRIVER_VERSION=${2:-2.12.1}
 echo "Executing ... "
 docker run --rm -e MONGODB_URI=${MONGODB_URI} \
     -v "$(pwd)":/workspace \
-    -w /workspace/dotnet start-dotnet \
+    -w /workspace/dotnet ghcr.io/mongodb-developer/get-started-csharp \
     "sed -i 's/\"MongoDB.Driver\" Version=\"[x0-9]\+\.[x0-9]\+\.[x0-9]\+\"/\"MongoDB.Driver\" Version=\"${DRIVER_VERSION}\"/g' /workspace/dotnet/getstarted.csproj; \
     dotnet run"
