e34fe6511770d38ec7eb602170fc7f9fac2ec3da
Wan Bachtiar <sindbach@gmail.com>
2021-07-12 15:38:47 +1000

    Updated to use MongoDB C# version 2.12.4

--- get-started.sh
+++ get-started.sh
@@ -5,7 +5,7 @@ then
     read -p "MONGODB URI (Required): " MONGODB_URI
 fi 
 
-DRIVER_VERSION=${2:-2.12.1}
+DRIVER_VERSION=${2:-2.12.4}
 echo "Executing ... "
 docker run --rm -e MONGODB_URI=${MONGODB_URI} \
     -v "$(pwd)":/workspace \
